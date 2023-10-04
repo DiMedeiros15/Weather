b05a2733e1d07c686b9ea4690d5d392fb5443896
Diego Medeiros <69081730+DiMedeiros15@users.noreply.github.com>
2023-10-04 18:38:21 -0300

    new v2

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let date = new Date()
+let hour = date.getHours()
+let background = document.querySelector('.background-container')
+
+
+updateBackground()
+
+function updateBackground() {
+
+    let backgroundContainer = document.querySelector('.background-container')
+
+    if(hour >= 0 && hour < 12) {
+        background.style.backgroundImage = 'url(img/dia.jpg)'
+        document.querySelector('.header h1').innerHTML = 'Olá, bom dia!'
+        backgroundContainer.style.setProperty('--background-color-before', 'rgba(0, 0, 0, 0.20)')
+    } else if (hour >= 12 && hour < 17) {
+        background.style.backgroundImage = 'url(img/tarde.jpg)'
+        document.querySelector('.header h1').innerHTML = 'Olá, boa tarde!'
+        backgroundContainer.style.setProperty('--background-color-before', 'rgba(0, 0, 0, 0.0)')
+    } else if(hour >= 17 && hour < 18 ) {
+        background.style.backgroundImage = 'url(img/finaltarde.jpg)'
+        let titulo = document.querySelector('.header h1')
+        titulo.innerHTML = 'Finalzinho de tarde...'
+        titulo.style.fontSize = '45px'
+        backgroundContainer.style.setProperty('--background-color-before', 'rgba(0, 0, 0, 0.40)')
+    } else {
+        background.style.backgroundImage = 'url(img/noite.jpg)'
+        document.querySelector('.header h1').innerHTML = 'Olá, boa noite!'
+        backgroundContainer.style.setProperty('--background-color-before', 'rgba(0, 0, 0, 0.60)')
+    }
+}
+
+setTimeout(()=>{
+    document.querySelector('.loadingContainer').style.display = 'none'
+    document.querySelector('.container').style.display = 'flex'
+},1300)
+
+setTimeout(()=>{
+    document.querySelector('.header h3').style.display = 'flex'
+},2300)
+
 document.querySelector('.busca').addEventListener('submit', async(event)=>{
     event.preventDefault();
 
@@ -20,29 +61,32 @@ document.querySelector('.busca').addEventListener('submit', async(event)=>{
                 temp: json.main.temp,
                 weather: json.weather[0].icon,
                 windSpeed: json.wind.speed,
-                windAngle: json.wind.deg
+                windAngle: json.wind.deg,
+                feelsLike: json.main.feels_like,
+                tempMax: json.main.temp_max,
+                tempMin: json.main.temp_min
             })
         }
         else{
             clearInfo()
             showMensage('Não encontramos essa localização.')
-        }
-    
-        }else{
-            clearInfo()
-        }
+        }}
     });
 
 function showInfo(json){
     showMensage('');
 
     document.querySelector('.titulo').innerHTML = `${json.name} - ${json.country}`
-    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
-    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`
+    document.querySelector('.tempInfo').innerHTML = `${json.temp.toFixed(0)} <sup>ºC</sup>`
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed.toFixed(1)} <span>km/h</span>`
     
-    document.querySelector('.temp > img').setAttribute('src',`http://openweathermap.org/img/wn/${json.weather}@2x.png`) 
+    document.querySelector('.temp img').setAttribute('src',`http://openweathermap.org/img/wn/${json.weather}@2x.png`) 
     
-    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`
+    document.querySelector('.wind-position').style.transform = `rotate(${json.windAngle}deg)`
+
+    document.querySelector('.feels-like-info').innerHTML = `${json.feelsLike.toFixed(0)}<sup>ºC</sup>`
+    document.querySelector('.temp-max-info').innerHTML = `${json.tempMax.toFixed(0)}<sup>ºC</sup>`
+    document.querySelector('.temp-min-info').innerHTML = `${json.tempMin.toFixed(0)}<sup>ºC</sup>`
 
     document.querySelector('.resultado').style.display = 'block';
 }
